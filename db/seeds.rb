14e7ebb9a8ea3fd8ca60730c88052c728e297a21
Tatiana <Bowmantatiana2@gmail.com>
2023-02-20 07:12:32 -0800

    commit tree a9d72779762ed3efff77b74da3fc5cb6517d1dd7

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding student data..."
+
+Student.create!(first_name: "Destiny", last_name: "Bowman", grade: 9)
+
+puts "Done! Student data has been seeded!"
